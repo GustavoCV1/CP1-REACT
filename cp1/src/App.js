ef07cf7a0f6a534beb1550c4df9bf4fa03bc6c0b
GustavoCV1 <gu.vasselai@gmail.com>
2021-08-27 09:42:52 -0300

    iniciando react/menu

---
+++ cp1/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className = "container--sm"></div>
+      <ul>
+        <li><a href="#home">Nacional</a></li>
+        <li><a href="#news">Business</a></li>
+        <li><a href="#contact">Internacional</a></li>
+        <li><a href="#about">Saúde</a></li>
+        <li><a href="#about">Tecnologia</a></li>
+        <li><a href="#about">Esporte</a></li>
+        <li><a href="#about">Entreterimento</a></li>
+        <li><a href="#about">Estilo</a></li>
+        <li><a href="#about">Viagem e Gastronomia</a></li>
+        <li><a href="#about">Newsletters</a></li>
+        <li><a href="#about">Podcasts</a></li>
+    </ul>
+    </div>
+  );
+}
+
+export default App;
